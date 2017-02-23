ad61c523f2cd368e5c228d67a94a1d8829de908f
dimas1976 <dm_kogan@yahoo.de>
2017-02-23 13:26:03 +0100

    Add files via upload

---
+++ preview.js
@@ -0,0 +1,10 @@
+/*
+export class Preview {
+
+    finishPreview(){
+        console.log('PREVIEW');
+        let preview = document.querySelector('#preview');
+        preview.style.display = 'none';
+    }
+}*/
+"use strict";
